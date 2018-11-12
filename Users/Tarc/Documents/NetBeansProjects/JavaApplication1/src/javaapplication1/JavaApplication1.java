e0d4a6739bbd82a55ec869cc4d039440a36e9e3e
Tarc <Tarc@B94>
2018-11-12 11:03:56 +0800

    test4

--- Users/Tarc/Documents/NetBeansProjects/JavaApplication1/src/javaapplication1/JavaApplication1.java
+++ Users/Tarc/Documents/NetBeansProjects/JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -4,7 +4,7 @@ package javaapplication1;
 public class JavaApplication1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World");        System.out.println("Hello World");
     }
     
 }
